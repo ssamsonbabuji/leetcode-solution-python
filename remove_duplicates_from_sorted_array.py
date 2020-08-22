ebc2ab9ed31b4e1df3e7e930de7c2c27f588d485
ssamsonbabuji <70076957+ssamsonbabuji@users.noreply.github.com>
2020-08-22 18:09:59 -0400

    Create remove_duplicates_from_sorted_array.py

---
+++ remove_duplicates_from_sorted_array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+    
+        new_nums = sorted(list(set(nums)))
+        
+        nums[:] = new_nums
+        
+        return len(new_nums)
