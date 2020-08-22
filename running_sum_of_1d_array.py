968282bbd8da3d66a20e506d1c921b909020ae36
ssamsonbabuji <70076957+ssamsonbabuji@users.noreply.github.com>
2020-08-22 19:17:20 -0400

    Update running_sum_of_1d_array.py

--- running_sum_of_1d_array.py
+++ running_sum_of_1d_array.py
@@ -1,7 +1,13 @@
 class Solution:
+    
     def runningSum(self, nums: List[int]) -> List[int]:
+        
         i = 1
+        
         while i<len(nums):
+            
             nums[i]+=nums[i-1]
+            
             i+=1
+            
         return nums
